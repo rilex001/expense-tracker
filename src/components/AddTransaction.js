9bd7436193ffc46fedcc0630696dbae77610c098
Nikola <ristic1900@gmail.com>
2020-05-22 23:33:28 +0200

    first commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const AddTransaction = () => {
+    return (
+        <div>
+            <h3>Add new transaction</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="text">Text</label>
+                    <input type="text" placeholder="Enter text..." />
+                </div>
+                <div className="form-control">
+                    
+                    <input type="number" placeholder="Enter amount..." />
+                </div>
+                <button className="btn">Add transaction</button>
+      </form>
+        </div>
+    )
+}
